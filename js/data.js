4e819a60611a02da0fa219bcc4f7bafcdf172a14
shneyderis <shneyderis@gmail.com>
2026-04-06 15:02:51 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 // js/data.js — Data loading: CSV, WooCommerce, NBU, Settings
 const SID="1K52LGIjxaQg1LfmjdVKXA0-wMh0MVUo16q-Z6BiNOsQ";
-const SID2="1pcg8U9NBF7PpjUi8AFm7L8o4-EEclG_ApbKvODl0mgA";
+const SID2="1NGC2iicXsEZRzOASBz2KZY46Lu7EO7jNd28UOWmQK7w";
 const WC="/api/wc";
 const WCK="consumer_key=ck_5b87215529858139d17b602945170ae4d9c8adbd&consumer_secret=cs_3ad054a505185162e849a92bf019979e6c037c93";
 
